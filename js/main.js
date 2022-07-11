db0233ef68f0455f6fa097b7b2d0be6e659bebfd
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-07-11 23:18:47 +0300

    sixteen commit

--- js/main.js
+++ js/main.js
@@ -1,91 +1,158 @@
-"use strict";
 
-var body = document.body,
-    animationItems = document.querySelectorAll("._animation-items"),
-    animationItemsSecond = document.querySelectorAll("._animation-items-second"),
-    videoIframes = ['<iframe width="560" height="315" src="https://www.youtube.com/embed/tRpoI6vkqLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/5sffVBeaOeY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/qz0aGYrrlhU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'],
-    swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: !0,
-  centeredSlides: !0,
-  slidesPerView: "auto",
-  initialSlide: 1,
-  speed: 500,
-  roundLengths: !0,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: !0
-  },
-  pagination: {
-    el: ".swiper-pagination"
-  }
+const body = document.body
+
+const animationItems = document.querySelectorAll('._animation-items')
+
+const animationItemsSecond = document.querySelectorAll('._animation-items-second')
+
+const videoIframes = [
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/tRpoI6vkqLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/5sffVBeaOeY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/qz0aGYrrlhU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+]
+
+
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    initialSlide: 1,
+    speed: 500,
+    roundLengths: true,
+    // freeModeMomentumVelocityRatio: 10,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+
+    },
+    pagination: {
+        el: ".swiper-pagination",
+
+    },
 });
 
-if ($(document).ready(function () {
-  var e = $(".courses__main-button");
-  $(".courses__item").each(function () {
-    $(this).hover(function () {
-      e.css({
-        "margin-top": "80px"
-      });
-    }, function () {
-      e.css({
-        "margin-top": "137px"
-      });
-    });
-  });
-}), animationItems.length > 0) {
-  var _animationOnScroll = function _animationOnScroll() {
-    for (var e = 0; e < animationItems.length; e++) {
-      var r = animationItems[e],
-          t = r.offsetHeight,
-          o = offset(r).top,
-          i = 1.5;
-      var a = window.innerHeight - t / i;
-      t > window.innerHeight && (a = window.innerHeight - window.innerHeight / i), scrollY > o - a && scrollY < o + t ? (r.classList.add("_active"), r.classList.add("stop-counting")) : r.classList.remove("_active");
+
+
+$(document).ready(function() {
+
+    const coursesMainButton = $('.courses__main-button')
+    $('.courses__item').each(function() {
+        $(this).hover(function() {
+            coursesMainButton.css({
+                'margin-top': '80px'
+            })
+        }, function() {
+            coursesMainButton.css({
+                'margin-top': '137px'
+            })
+        })
+    })
+})
+
+if (animationItems.length > 0) {
+    window.addEventListener('wheel', animationOnScroll)
+    window.addEventListener('scroll', animationOnScroll)
+    function animationOnScroll() {
+        for (let index = 0; index < animationItems.length; index++) {
+            const animationItem = animationItems[index]
+            const animationItemHeight = animationItem.offsetHeight
+            const animationItemOffset = offset(animationItem).top
+            const animationStart = 4
+
+            let animationItemPoint = window.innerHeight - animationItemHeight / animationStart
+
+            if (animationItemHeight > window.innerHeight) {
+                animationItemPoint = window.innerHeight - window.innerHeight / animationStart
+            }
+            if ((scrollY > (animationItemOffset - animationItemPoint)) && (scrollY < (animationItemOffset + animationItemHeight))) {
+                animationItem.classList.add('_active')
+                animationItem.classList.add('stop-counting')
+            } else {
+                animationItem.classList.remove('_active')
+            }
+        }
+        if (animationItemsSecond.length > 0) {
+            for (let index = 0; index < animationItemsSecond.length; index++) {
+                const animationItem = animationItemsSecond[index]
+                const animationItemHeight = animationItem.offsetHeight
+                const animationItemOffset = offset(animationItem).top
+                const animationStart = 2.5
+
+                let animationItemPoint = window.innerHeight - animationItemHeight / animationStart
+
+                if (animationItemHeight > window.innerHeight) {
+                    animationItemPoint = window.innerHeight - window.innerHeight / animationStart
+                }
+                if ((scrollY > (animationItemOffset - animationItemPoint)) && (scrollY < (animationItemOffset + animationItemHeight))) {
+                    animationItem.classList.add('_active-second')
+                    animationItem.classList.add('stop-counting')
+                } else {
+                    animationItem.classList.remove('_active-second')
+                }
+            }
+        }
     }
-
-    if (animationItemsSecond.length > 0) for (var _e = 0; _e < animationItemsSecond.length; _e++) {
-      var _r = animationItemsSecond[_e],
-          _t = _r.offsetHeight,
-          _o = offset(_r).top,
-          _i = 2;
-
-      var _a = window.innerHeight - _t / _i;
-
-      _t > window.innerHeight && (_a = window.innerHeight - window.innerHeight / _i), scrollY > _o - _a && scrollY < _o + _t ? (_r.classList.add("_active-second"), _r.classList.add("stop-counting")) : _r.classList.remove("_active-second");
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollY || document.documentElement.scrollLeft,
+            scrollTop = window.scrollX || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
     }
-  };
-
-  var offset = function offset(e) {
-    var r = e.getBoundingClientRect(),
-        t = window.scrollY || document.documentElement.scrollLeft,
-        o = window.scrollX || document.documentElement.scrollTop;
-    return {
-      top: r.top + o,
-      left: r.left + t
-    };
-  };
-
-  window.addEventListener("wheel", _animationOnScroll), window.addEventListener("scroll", _animationOnScroll);
 }
 
-var headerLang = body.querySelector(".header__lang"),
-    burgerLogo = body.querySelector(".burger__logo"),
-    headerBurger = body.querySelector(".header__burger"),
-    burgerItems = body.querySelector(".burger__items"),
-    burgerCloseBtn = body.querySelector(".burger__label img");
-headerLang.addEventListener("click", function () {
-  headerLang.classList.toggle("header__lang-passive"), headerLang.classList.toggle("header__lang-active");
-}), burgerLogo.addEventListener("click", function () {
-  headerBurger.classList.toggle("header__burger-passive"), headerBurger.classList.toggle("header__burger-active"), headerBurger.classList.contains("header__burger-active") ? body.classList.add("overflow-hidden") : body.classList.remove("overflow-hidden");
-}), burgerItems.querySelectorAll("a").forEach(function (e) {
-  e.addEventListener("click", function () {
-    headerBurger.classList.toggle("header__burger-passive"), headerBurger.classList.toggle("header__burger-active"), headerBurger.classList.contains("header__burger-active") ? body.classList.add("overflow-hidden") : body.classList.remove("overflow-hidden");
-  });
-}), burgerCloseBtn.addEventListener("click", function () {
-  headerBurger.classList.toggle("header__burger-passive"), headerBurger.classList.toggle("header__burger-active"), headerBurger.classList.contains("header__burger-active") ? body.classList.add("overflow-hidden") : body.classList.remove("overflow-hidden");
-}), setTimeout(animationOnScroll, 500);
+const headerLang = body.querySelector('.header__lang')
+
+const burgerLogo = body.querySelector('.burger__logo')
+
+const headerBurger = body.querySelector('.header__burger')
+
+const burgerItems = body.querySelector('.burger__items')
+
+const burgerCloseBtn = body.querySelector('.burger__label img')
+
+
+headerLang.addEventListener('click', () => {
+    headerLang.classList.toggle('header__lang-passive')
+    headerLang.classList.toggle('header__lang-active')
+})
+
+burgerLogo.addEventListener('click', () => {
+    headerBurger.classList.toggle('header__burger-passive')
+    headerBurger.classList.toggle('header__burger-active')
+    if (headerBurger.classList.contains('header__burger-active')) {
+        body.classList.add('overflow-hidden')
+    } else {
+        body.classList.remove('overflow-hidden')
+    }
+})
+
+
+burgerItems.querySelectorAll('a').forEach(el => {
+    el.addEventListener('click', () => {
+        headerBurger.classList.toggle('header__burger-passive')
+        headerBurger.classList.toggle('header__burger-active')
+        if (headerBurger.classList.contains('header__burger-active')) {
+            body.classList.add('overflow-hidden')
+        } else {
+            body.classList.remove('overflow-hidden')
+        }
+    })
+})
+
+burgerCloseBtn.addEventListener('click', () => {
+    headerBurger.classList.toggle('header__burger-passive')
+    headerBurger.classList.toggle('header__burger-active')
+    if (headerBurger.classList.contains('header__burger-active')) {
+        body.classList.add('overflow-hidden')
+    } else {
+        body.classList.remove('overflow-hidden')
+    }
+})
+
+
+
+setTimeout(animationOnScroll, 200)
