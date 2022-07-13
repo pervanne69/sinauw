5863f588136b1b1756190bafb5795a55e6f09e0f
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-07-13 23:42:37 +0300

    twenty seven commit

--- js/main.js
+++ js/main.js
@@ -129,7 +129,7 @@ window.addEventListener('resize', () => {
             const footerTitle = element.querySelector('.footer__title')
             footerTitle.addEventListener('click', () => {
                 element.classList.toggle('footer__list-body-passive')
-                element.classList.toggle('footer__list-body-passive')
+                element.classList.toggle('footer__list-body-active')
             })
         })
     }
